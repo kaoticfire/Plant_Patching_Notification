0c8f791750b9c87b392b6f1ad8e3ca6ee17db03f
V <virgilhoover@GMAIL.COM>
2023-08-17 09:03:36 -0600

    Removed un-needed code that was commented out

--- src/config.py
+++ src/config.py
@@ -7,10 +7,5 @@ class Config:
     DEBUG = False
     CHANGE_TICKET = 'CHG0046567'
     WEEK_NUMBER = 1
-    # TIMEZONE = 'Eastern'
     DAYS = 13
     SPOKE_TO = ''
-    # ZONE = {'Eastern': 'EDT',
-    #        'Central': 'CDT',
-    #        'Mountain': 'MDT',
-    #        'Pacific': 'PDT'}
